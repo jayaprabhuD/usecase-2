d85fd4da5a9b8356e0c06b3c212ea04328300d8c
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-02 12:10:56 +0000

    Added rds SG

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -38,3 +38,23 @@ resource "aws_security_group" "bayer_app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# 3. Creating security group for Application Instances
+resource "aws_security_group" "bayer_rds_sg" {
+  name   = "bayer-rds-sg"
+  vpc_id = aws_vpc.bayer_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bayer_app_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
