e2b856dc0693299e2149c4400dd7657fe996383a
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-02 14:54:16 +0000

    Correction

--- resources.tf
+++ resources.tf
@@ -2,22 +2,22 @@ module "vpc" {
   source = "./modules/vpc"
 }
 
-module "security_groups" {
-  source     = "./modules/security_groups"
-}
-
-module "application_instances" {
-  source     = "./modules/application_instances"
-}
-
-module "target_group" {
-  source      = "./modules/target_group"
-}
-
-module "alb" {
-  source      = "./modules/alb"
-}
-
-module "rds" {
-  source      = "./modules/rds"
-}
+#odule "security_groups" {
+# source     = "./modules/security_groups"
+#
+#
+#odule "application_instances" {
+# source     = "./modules/application_instances"
+#
+#
+#odule "target_group" {
+# source      = "./modules/target_group"
+#
+#
+#odule "alb" {
+# source      = "./modules/alb"
+#
+#
+#odule "rds" {
+# source      = "./modules/rds"
+#
